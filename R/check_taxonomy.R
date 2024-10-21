7a0a481b8fa653162fe9b17f7869a8fa4143ae6c
Francisco Rodriguez-Sanchez <f.rodriguez.sanc@gmail.com>
2024-10-22 01:12:29 +0200

    simplify

--- R/check_taxonomy.R
+++
@@ -1,8 +0,0 @@
-check_taxonomy <- function(df) {
-
-  require(assertr)
-
-  rgbif::name_backbone_checklist(df$species) |>
-    assert(in_set("EXACT", "FUZZY"), matchType)
-
-}
---
+++ process_data.R
@@ -0,0 +1,26 @@
+
+## Import data
+df <- gsheet::gsheet2tbl("https://docs.google.com/spreadsheets/d/1oEp3LAcXLt5frr3dverde1YjwUg1fihVtEXKwaLJ_ns/edit?usp=sharing")
+
+
+## Check values
+
+library(assertr)
+
+df |>
+  chain_start() |>
+  assert(in_set("black", "red"), fruit_colour) |>
+  assert(within_bounds(0, 5), fruit_weight_g) |>
+  chain_end()
+
+
+## Check taxonomy
+
+rgbif::name_backbone_checklist(df$species) |>
+  assert(in_set("EXACT", "FUZZY"), matchType)
+
+
+## Append new data to database
+write.csv(df, "database.csv", append = TRUE)
+
+
